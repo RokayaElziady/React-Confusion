c485064e0e5e0838795ee7829130737522f3111d
Rokaya Elziady <rokayaelziady@gmail.com>
2020-07-07 00:07:26 +0200

    fetching from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {Card ,CardImg,CardImgOverlay,CardBody,CardText,CardTitle, Container,Brea
 import {Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -104,7 +105,7 @@ class CommentForm extends Component{
     
         return(
                <Card>
-                  <CardImg width="100%"  top src={dish.image} alt={dish.name} />
+                   <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                   <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
